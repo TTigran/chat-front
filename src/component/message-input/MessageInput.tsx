7988e57b2bc9dc6a93230bf4a2ccc11071a2aa3f
Tigran Martirosyan <tigran.martirosian.1994.2016@gmail.com>
2025-08-20 16:53:06 +0400

    redisayn

--- src/component/message-input/MessageInput.tsx
+++ src/component/message-input/MessageInput.tsx
@@ -1,21 +1,41 @@
 import React, { useState } from 'react';
-import './index.css'
+import './index.css';
+import Picker from 'emoji-picker-react';
 
 const MessageInput = ({send}: { send: (val: string, e: any) => void }) => {
-    const [value, setValue] = useState("")
+    const [value, setValue] = useState("");
+    const [showEmoji, setShowEmoji] = useState(false);
+
+    const onEmojiClick = (emojiObject: any) => {
+        setValue(prev => prev + emojiObject.emoji);
+    };
 
     return (
         <div className='message-input-container'>
+
+
             <input
                 className="message-input"
                 onChange={(e)=>setValue(e.target.value)}
                 placeholder="Type your message..."
                 value={value}
             />
+
             <button
                 className='send-button'
                 onClick={(e) => send(value,e)}
             >Send</button>
+
+            <button
+                className="emoji-button"
+                onClick={() => setShowEmoji(!showEmoji)}
+            >😊</button>
+
+            {showEmoji && (
+                <div className="emoji-picker-wrapper">
+                    <Picker onEmojiClick={onEmojiClick} />
+                </div>
+            )}
         </div>
     );
 };
